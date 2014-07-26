eced8b77c39233a5e470a968fb67683cf160a10c
FourBit <razershockz@gmail.com>
2014-07-26 17:49:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ Please refer to the Readme.md for license stuff
 */
 (function () {
     var motdMsg = ["Welcome to the FourBit plug.dj room!"];
-    var joinTime = Date.getTime();
+    var joinTime = Date.now();
     var blacklist = ['#SELFIE (Official Music Video)', 'Troll Song'];
     var settings = {
         woot: true,
@@ -568,7 +568,7 @@ Please refer to the Readme.md for license stuff
                     break;
                 case 'status':
                     if (check()) {
-                        var z = Date.getTime();
+                        var z = Date.now();
                         var y = Math.floor(joinTime - z);
                         API.sendChat('/em [' + from + '] Uptime: ' + y + ' ~ Party: ' + settings.party + ' ~ Blacklist: ' + settings.blacklist);
                     }
