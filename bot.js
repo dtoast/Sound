7f4d4c12a3c86ee244a491f227249a373c883f75
FourBit <razershockz@gmail.com>
2014-10-06 16:51:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,4 @@
 /*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
-
-/*
 	Copyright (c) 2014 FourBit.
 	
 	If you want to modify, please
